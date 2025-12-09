4bfb0acec4a4afce88b409ebafeef3d28fbea7f2
Daniel Tucker <70220762+Vision940@users.noreply.github.com>
2025-12-09 00:40:26 -0600

    Update package build action

--- container/build-image.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# This script is here for ease of understanding container implementation
-sudo docker build -t boom:latest . || exit 1
-sudo docker save boom:latest -o boom-image.tar || exit 1
-
-echo Load image with \`docker load -i boom-image.tar\`
-
